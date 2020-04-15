704dae1ba3725c2ee196274763ebed7d96132d70
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-15 13:12:47 -0400

    learning PropTypes

--- src/components/movies/movie-list.js
+++ src/components/movies/movie-list.js
@@ -1,12 +1,24 @@
-import React from "react";
-
-const MovieList = (props) => {
-  return (
-    <div>
-      <h1>{props.movie.name}</h1>
-      <h5>{props.movie.gene}</h5>
-    </div>
-  );
-};
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+class MovieList extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
+    desc: PropTypes.string,
+  };
+  static defaultProps = {
+    desc: " Not available",
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.props.movie.name}</h1>
+        <h3>{this.props.desc}</h3>
+        <h5>{this.props.movie.gene}</h5>
+      </div>
+    );
+  }
+}
 
 export default MovieList;
--- src/components/practise/loop.js
+++ src/components/practise/loop.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import MovieList from "../movies/movie-list";
 const movies = [
-  { id: 1, name: "starwar", gene: "pg" },
+  { id: 1, name: "Tizi", gene: "pg" },
   { id: 2, name: "starwar2", gene: "pg" },
-  { id: 3, name: "starwar3", gene: "pg" },
-  { id: 4, name: "starwar4", gene: "pg" },
+  { id: 3, name: "starwar3", gene: "pg", desc: "" },
+  { id: 4, name: "starwar4", gene: "pg", desc: "adult" },
 ];
 class Loop extends Component {
   render() {
     return (
       <>
         {movies.map((movie) => {
-          return <MovieList key={movie.id} movie={movie}></MovieList>;
+          return (
+            <MovieList
+              key={movie.id}
+              movie={movie}
+              desc={movie.desc}
+            ></MovieList>
+          );
         })}
       </>
     );
