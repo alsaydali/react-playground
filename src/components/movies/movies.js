58280484a056cdaede139bad7a0b93d84a78c1e5
Marwan Alsaydali <alsaydali@icloud.com>
2020-04-20 20:45:43 -0400

    added the image

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
 
 const Movies = (props) => (
   <div>
+    <img src={`${POSTER_PATH}${props.movie.poster_path}`} />
     <h1>{props.movie.title}</h1>
   </div>
 );
